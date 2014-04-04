71d3fccc8a6af16760c37561d6c8dce4bc35cf5a
miso dengaku <misodengaku@gmail.com>
2014-04-05 04:42:15 +0900

    first commit

---
+++ maki/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maki
+{
+	public enum μs
+	{
+		穂乃果, 絵里, ことり, 海未, 凛, 真姫, 希, 花陽, にこ
+	}
+
+	public class SchoolIdol
+	{
+		public int 年齢 { get; private set; }
+		public int 彼氏いない歴 { get; private set; }
+		public string 趣味 { get; private set; }
+		public string 特技 { get; private set; }
+		public string 今1番欲しい物 { get; private set; }
+		// 続く
+
+		public SchoolIdol(μs name)
+		{
+			switch(name)
+			{
+				case μs.絵里:
+				case μs.にこ:
+				case μs.希:
+					年齢 = 17;
+					彼氏いない歴 = 年齢;
+					break;
+				case μs.海未:
+				case μs.穂乃果:
+				case μs.ことり:
+					年齢 = 16;
+					彼氏いない歴 = 年齢;
+					break;
+				case μs.花陽:
+				case μs.凛:
+					年齢 = 15;
+					彼氏いない歴 = 年齢;
+					break;
+				case μs.真姫:
+					// 他は知らん
+					年齢 = 15;
+					彼氏いない歴 = 17;
+					趣味 = "買い物";
+					特技 = "開脚";
+					今1番欲しい物 = "お兄ちゃん";
+					break;
+
+			}
+		}
+	}
+}
---
+++ maki/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maki
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var maki = new SchoolIdol(μs.真姫);
+			Console.WriteLine("マキ、" + maki.年齢 + "歳♡\n将来はパパとママの跡を継いで医者になるつもりだから、本当はアイ活してる時間なんて本当はないんだけど、こーんなにカワイイのにアイドルにならないなんてもったいないじゃナイ？だからとりあえず、青春の思い出に参戦♡趣味は" +maki.趣味+ "、今1番欲しい物は"+maki.今1番欲しい物+"、特技は" +maki.特技+"、あとは──アピールポイントはなんだっけな。あ、こう見えて彼氏いない歴" + maki.彼氏いない歴 + "年よ！");
+			Console.ReadLine();
+		}
+	}
+}
